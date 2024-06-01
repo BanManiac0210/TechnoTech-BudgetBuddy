ae81721abf56ee3cb3ca1ccb9ddf17b0f8f1bb7a
RenIVII <motcongmotbangba1975@gmail.com>
2024-06-01 11:51:36 +0700

    something

--- src/constants/fakedata.js
+++ src/constants/fakedata.js
@@ -2,6 +2,60 @@ const FDATA = {
     currentBalance: 14200000,
     incomeValue: 350000,
     expenseValue: 4500000,
+    tagName: "Ăn uống",
+    tagIconName: "cutlery",
+    mainHistory: [
+        {
+          "sourceName": "Tiền mặt",
+          "sourceIcon": "money",
+          "cateName": "Ăn uống",
+          "cateIcon": "cutlery",
+          "date": "2024-06-01",
+          "value": 25000,
+          "description": "Cơm ktx",
+          "type": "Chi tiêu"
+        },
+        {
+            "sourceName": "Tiền mặt",
+            "sourceIcon": "money",
+            "cateName": "Ăn uống",
+            "cateIcon": "cutlery",
+            "date": "2024-06-01",
+            "value": 25000,
+            "description": "Cơm ktx",
+            "type": "Chi tiêu"
+        },
+        {
+            "sourceName": "Tiền mặt",
+            "sourceIcon": "money",
+            "cateName": "Ăn uống",
+            "cateIcon": "cutlery",
+            "date": "2024-06-01",
+            "value": 25000,
+            "description": "Cơm ktx",
+            "type": "Chi tiêu"
+        },
+        {
+            "sourceName": "Tiền mặt",
+            "sourceIcon": "money",
+            "cateName": "Ăn uống",
+            "cateIcon": "cutlery",
+            "date": "2024-06-01",
+            "value": 25000,
+            "description": "Cơm ktx",
+            "type": "Chi tiêu"
+        },
+        {
+            "sourceName": "Tiền mặt",
+            "sourceIcon": "money",
+            "cateName": "Ăn uống",
+            "cateIcon": "cutlery",
+            "date": "2024-06-01",
+            "value": 25000,
+            "description": "Cơm ktx",
+            "type": "Chi tiêu"
+        }
+    ]
 };
 
 export {FDATA}
